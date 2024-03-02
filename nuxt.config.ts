292bd00e01846e5e44ef9a363842edafdafc6f48
florent baris <florent.baris@epitech.eu>
2024-03-02 18:38:19 +0900

    Essai 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  ssr: false,
   image: {
     dir: 'assets/images',
   },
--- pages/game/[id].vue
+++ pages/game/[id].vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col overflow-hidden rounded-md">
         <a>
-            <NuxtImg :src="house?.image" :alt="house?.name" width="600" class="h-[200px] object-cover rounded-md" />
+            <!-- <NuxtImg :src="house?.image" :alt="house?.name" width="600" class="h-[200px] object-cover rounded-md" /> -->
             <div class="flex flex-col py-2 pl-1 text-black">
                 <div class="flex items-center justify-between">
                     <h3 class="text-xl font-bold truncate overflow-hidden max-w-[200px]">{{ house?.name }}</h3>
