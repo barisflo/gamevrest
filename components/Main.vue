5a15819d9e492972f7133dbd1c265b5bc5f1c80b
florent baris <florent.baris@epitech.eu>
2024-03-09 21:19:46 +0900

    Image size + meta

--- components/Main.vue
+++ components/Main.vue
@@ -2,12 +2,12 @@
   <div class="bg-cake min-w-full py-7" :style="{ backgroundImage: `url('/cake1.png')` }">
     <!-- <NuxtImg class=" absolute p-2 -z-10" src="/cake1.png" alt="Gamevrest logo" width="600" /> -->
     <div class="flex flex-wrap justify-center">
-      <NuxtImg class="p-2" src="/coverart.png" alt="Gamevrest logo" width="600" />
-      <NuxtImg class="p-2" src="/grenouille.png" alt="Gamevrest logo" width="600" />
+      <NuxtImg class="p-2" src="/coverart.png" alt="Premier Servi Cover art" width="600" />
+      <NuxtImg class="p-2" src="/grenouille.png" alt="Premier Servi Gameplay Grenouille" width="600" />
     </div>
     <div class="flex flex-wrap  justify-center">
-      <NuxtImg class="p-2" src="/select-menu.png" alt="Gamevrest logo" width="600" />
-      <NuxtImg class="p-2" src="/tuto.png" alt="Gamevrest logo" width="600" />
+      <NuxtImg class="p-2" src="/select-menu.png" alt="Premier Servi select menu" width="600" />
+      <NuxtImg class="p-2" src="/tuto.png" alt="Premier Servi Tutorial" width="600" />
     </div>
   </div>
 </template>
