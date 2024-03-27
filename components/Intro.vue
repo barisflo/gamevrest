567699d62b4ce85d1114c1e3800c26b091469ae2
baris-f <florent.baris@epitech.eu>
2024-03-27 13:58:19 +0900

    steam button

--- components/Intro.vue
+++ components/Intro.vue
@@ -5,7 +5,10 @@
             <h1
                 class="text-3xl md:text-5xl font-extrabold leading-tighter tracking-tighter pb-2 mb-4 bg-clip-text text-transparent bg-gradient-to-br from-yellow-200 to-rose-300">
                 Eat the cake before your friends without getting spotted !</h1>
-            <a href="https://store.steampowered.com/app/2884670/Premier_Servi/?beta=0" class="text-3xl text-gray-900 mb-4"> Switch • Steam</a>
+            <a href="https://store.steampowered.com/app/2884670/Premier_Servi/?beta=0"><NuxtImg class="p-2 m-auto hover:scale-105 ease-in-out duration-100" src="/steam.png" alt="Steam wishlist" width="300" />
+                </a>
+            <a href="https://store.steampowered.com/app/2884670/Premier_Servi/?beta=0"
+                class="text-xl text-gray-900 mb-4"> Switch • Steam</a>
             <div class="max-w-3xl mx-auto">
                 <p class="text-xl text-gray-500 mb-4 mt-2" data-aos="zoom-y-out" data-aos-delay="150">Developed by 2
                     young french hobbyist, Premier Servi (First Arrived, First Served) is a colorful Party Game
