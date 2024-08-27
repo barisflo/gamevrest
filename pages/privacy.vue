cae6709f600e511eeab4d1bcd3a7e166bec03828
baris-f <florent.baris@epitech.eu>
2024-08-27 18:03:50 +0900

    Create privacy.vue

---
+++ pages/privacy.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full max-w-5xl mx-auto flex flex-col justify-center">
+        <div class="max-w-[185rem] px-4 sm:px-6 lg:px-8 mx-auto">
+            <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+                <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Privacy Policy</h2>
+                <p class="mt-4 text-gray-600 dark:text-gray-400">
+                    At Gamevrest, we do not collect personal or sensitive user data. Any data we collect is limited to non-personal information like device type and in-game interactions, strictly for improving game performance and user experience. We ensure full transparency in how this data is accessed, collected, used, and handled.
+                </p>
+                <p class="mt-4 text-gray-600 dark:text-gray-400">
+                    <strong>Data We Collect:</strong> Our games collect only non-personal data, such as device type and in-game actions. This data is used solely to enhance game performance and user experience. We do not collect, use, or share any personal or sensitive data.
+                </p>
+                <p class="mt-4 text-gray-600 dark:text-gray-400">
+                    <strong>Third-Party Services:</strong> We use third-party services like ads and analytics in our games. These services might collect some data, but it’s not personal data. We ensure that any third-party code in our games complies with existing policies.
+                </p>
+                <p class="mt-4 text-gray-600 dark:text-gray-400">
+                    <strong>Policy Updates:</strong> We might update this policy as needed. Any changes will be reflected on this page.
+                </p>
+                <p class="mt-4 text-gray-600 dark:text-gray-400">
+                    <strong>Contact Us:</strong> If you have questions or concerns about our Privacy Policy, please visit our <a href="/contact" class="text-primary">Contact Us</a> page.
+                </p>
+                <p class="mt-8 text-gray-600 dark:text-gray-400">
+                    Thanks for playing with Gamevrest!
+                </p>
+                <a href="/"><div class="w-[140px] mx-auto my-5 p-2 h-12 bg-primary text-2xl font-bold text-center rounded-md text-white">
+                    Home</div></a>
+            </div>
+        </div>
+    </div>
+</template>
