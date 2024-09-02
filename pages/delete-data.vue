ff2c74922995ad7ca62d17ece2b1d307d8131bd0
baris-f <florent.baris@epitech.eu>
2024-09-02 13:01:19 +0900

    delete data update

---
+++ pages/delete-data.vue
@@ -0,0 +1,23 @@
+<template>
+        <div class="w-full max-w-5xl mx-auto flex flex-col justify-center">
+        <div class="max-w-[185rem] px-4 sm:px-6 lg:px-8 mx-auto">
+            <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+                <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white mb-2">Delete User Data</h2>
+    <div>
+        <p>We do not collect any personal data ourselve as mentionned in our <a href="/contact" class="text-primary">Privacy</a></p>
+        <p>However we use Unity ads which might collect data, and the only way to ask for it to delete it is to send an email to <a href="mailto:DPO@unity3d.com" class="text-primary">DPO@unity3d.com</a></p>
+        <p>Click <a href="https://unity.com/legal/privacy-policy" class="text-primary">here</a> to learn more about unity policy</p>
+    
+        <a href="/"><div class="w-[140px] mx-auto my-5 p-2 h-12 bg-primary text-2xl font-bold text-center rounded-md text-white">
+            Home</div></a>
+        </div>
+</div>
+    </div>
+</div>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue';
+
+  </script>
+  
