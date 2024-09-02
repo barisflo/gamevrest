719098e96605308d6334ce651f5e3630e4990a63
baris-f <florent.baris@epitech.eu>
2024-09-02 13:03:38 +0900

    privacy mistake

--- pages/delete-data.vue
+++ pages/delete-data.vue
@@ -4,7 +4,7 @@
             <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
                 <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white mb-2">Delete User Data</h2>
     <div>
-        <p>We do not collect any personal data ourselve as mentionned in our <a href="/contact" class="text-primary">Privacy</a></p>
+        <p>We do not collect any personal data ourselve as mentionned in our <a href="/privacy" class="text-primary">Privacy</a></p>
         <p>However we use Unity ads which might collect data, and the only way to ask for it to delete it is to send an email to <a href="mailto:DPO@unity3d.com" class="text-primary">DPO@unity3d.com</a></p>
         <p>Click <a href="https://unity.com/legal/privacy-policy" class="text-primary">here</a> to learn more about unity policy</p>
     
