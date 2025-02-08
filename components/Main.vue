eb3bbe26a0f192673395b931dee2adc6ab555efc
barisflo <florent.baris@epitech.eu>
2025-02-08 21:14:03 +0900

    update img

--- components/Main.vue
+++ components/Main.vue
@@ -3,11 +3,11 @@
     <!-- <NuxtImg class=" absolute p-2 -z-10" src="/cake1.png" alt="Gamevrest logo" width="600" /> -->
     <div class="flex flex-wrap justify-center">
       <NuxtImg class="p-2" src="/coverart.png" alt="Premier Servi Cover art by Florent Baris for Gamevrest" width="600" />
-      <NuxtImg class="p-2" src="/grenouille.png" alt="Premier Servi Gameplay Grenouille" width="600" />
+      <NuxtImg class="p-2" src="/blu.png" alt="Premier Servi Gameplay Grenouille" width="600" />
     </div>
     <div class="flex flex-wrap  justify-center">
-      <NuxtImg class="p-2" src="/select-menu.png" alt="Premier Servi select menu" width="600" />
-      <NuxtImg class="p-2" src="/tuto.png" alt="Premier Servi Tutorial" width="600" />
+      <NuxtImg class="p-2" src="/green.png" alt="Premier Servi select menu" width="600" />
+      <NuxtImg class="p-2" src="/red.png" alt="Premier Servi Tutorial" width="600" />
     </div>
     <div class="flex flex-wrap  justify-center">
       Indie Game studio Gamevrest by Florent Baris, Gaspard Thirion and Mickaël Scherlen
